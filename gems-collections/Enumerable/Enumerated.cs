dc7518e411f24cf2184b2f2f25b99617d20aaea7
thinkingmedia <mathew@thinkingmedia.ca>
2014-09-10 18:27:46 -0400

    some more basic features.

---
+++ gems-collections/Enumerable/Enumerated.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace gems_collections.Enumerable
+{
+    public static class Enumerated
+    {
+        public static IEnumerable<T> NotNull<T>(this IEnumerable<T> pThis) where T : class
+        {
+            return pThis.Where(pItem=>pItem != null);
+        }
+
+        /// <summary>
+        /// Filters the list so it doesn't contain any items from the other list.
+        /// </summary>
+        public static IEnumerable<T> ExceptThese<T>(this IEnumerable<T> pThis, IEnumerable<T> pThese)
+        {
+            return pThis.Where(pItem=>!pThese.Contains(pItem));
+        }
+
+        /// <summary>
+        /// Filters the list so it only contains items found in the other list.
+        /// </summary>
+        public static IEnumerable<T> OnlyThese<T>(this IEnumerable<T> pThis, IEnumerable<T> pThese)
+        {
+            return pThis.Where(pThese.Contains);
+        }
+    }
+}
---
+++ gems-collections/Enumerable/Strings.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace gems_collections.Enumerable
+{
+    public static class Strings
+    {
+        /// <summary>
+        /// Only strings with a non-whitespace value.
+        /// </summary>
+        public static IEnumerable<string> NotNullOrEmpty(this IEnumerable<string> pStrings)
+        {
+            return pStrings.Where(pItem=>!string.IsNullOrWhiteSpace(pItem));
+        }
+
+        /// <summary>
+        /// Only the strings that end with.
+        /// </summary>
+        public static IEnumerable<string> EndsWith(this IEnumerable<string> pStrings, string pWhat)
+        {
+            return pStrings.Where(pStr=>pStr.EndsWith(pWhat));
+        }
+
+        /// <summary>
+        /// Only the strings that start with.
+        /// </summary>
+        public static IEnumerable<string> StartsWith(this IEnumerable<string> pStrings, string pWhat)
+        {
+            return pStrings.Where(pStr=>pStr.StartsWith(pWhat));
+        }
+
+        /// <summary>
+        /// Makes all strings lower case.
+        /// </summary>
+        public static IEnumerable<string> ToLower(this IEnumerable<string> pStrings)
+        {
+            return pStrings.Select(pStr=>pStr.ToLower());
+        }
+
+        /// <summary>
+        /// Makes all strings upper case.
+        /// </summary>
+        public static IEnumerable<string> ToUpper(this IEnumerable<string> pStrings)
+        {
+            return pStrings.Select(pStr=>pStr.ToUpper());
+        }
+
+        /// <summary>
+        /// Trims all strings.
+        /// </summary>
+        public static IEnumerable<string> Trim(this IEnumerable<string> pStrings)
+        {
+            return pStrings.Select(pStr=>pStr.Trim());
+        }
+    }
+}
